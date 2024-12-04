6477db98b94c2602bd0ad16ff775955ca72bb784
Soyeon Lee <pink731p@gmail.com>
2024-12-04 19:19:52 +0900

    feat: winner 결정하는 기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -13,13 +13,20 @@ export default function Board() {
    const [xIsNext, setXIsNext] = useState(true);
    const [squares, setSquares] = useState(Array(9).fill(null));
 
-   const handleClick = (i) => {
-      const nextSqaures = squares.slice();
+   const winner = calculateWinner(squares);
+   let status;
+   if (winner) {
+      status = 'Winner: ' + winner;
+   } else {
+      status = 'Next Player: ' + (xIsNext ? 'X' : 'O');
+   }
 
-      if (squares[i]) {
+   const handleClick = (i) => {
+      if (squares[i] || calculateWinner(squares)) {
          return;
       }
 
+      const nextSqaures = squares.slice();
       if (xIsNext) {
          nextSqaures[i] = 'X';
       } else {
@@ -31,6 +38,7 @@ export default function Board() {
 
    return (
       <>
+         <div className="status">{status}</div>
          <div className="board-row">
             <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
             <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -48,4 +56,28 @@ export default function Board() {
          </div>
       </>
    );
+}
+
+function calculateWinner(squares) {
+   const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+   ];
+
+   const winningLine = lines.find(
+      ([a, b, c]) =>
+         squares[a] && squares[a] === squares[b] && squares[a] === squares[c]
+   );
+
+   if (winningLine) {
+      return squares[winningLine[0]];
+   }
+
+   return null;
 }
